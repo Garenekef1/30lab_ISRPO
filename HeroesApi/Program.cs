3c2cb9e1710b5a0581f274151c7cc1efe6f11946
Алексей Назаренко <Garenekef1@users.noreply.github.com>
2026-05-08 03:19:10 +0300

    Step 3: JSON configured - camelCase, enum as string

--- HeroesApi/Program.cs
+++ HeroesApi/Program.cs
@@ -1,6 +1,15 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
